af4d85ce04b29100e98cca171cdd0d0f24347116
liuup <akarealup@gmail.com>
2024-10-04 21:47:43 +0800

    modify distance

--- distance.py
+++ distance.py
@@ -14,9 +14,22 @@ def model_l1(model1, model2):
     pass
 
 # 测量两个模型层间的余弦相似度cossim
-def model_layer_cossim(model1, model2):
-    pass
+def layer_cossim(model1, model2, layers):
+    ans = []
+    all_layers = [name for name, _ in model1.named_parameters()]
+    for layer in layers:
+        if (layer+"bias") in all_layers:
+            layer_t_1 = torch.concat([model1.state_dict()[layer+".weight"].flatten(), 
+                                    model1.state_dict()[layer+".bias"].flatten()])
+            layer_t_2 = torch.concat([model2.state_dict()[layer+".weight"].flatten(), 
+                                    model2.state_dict()[layer+".bias"].flatten()])
+            ans.append(F.cosine_similarity(layer_t_1.unsqueeze(0), layer_t_2.unsqueeze(0)).item())
+        else:
+            layer_t_1 = model1.state_dict()[layer+".weight"].flatten()
+            layer_t_2 = model2.state_dict()[layer+".weight"].flatten()
+            ans.append(F.cosine_similarity(layer_t_1.unsqueeze(0), layer_t_2.unsqueeze(0)).item())
+    return ans
 
 # 测量两个模型层间的l1距离
-def model_layer_l1(model1, model2):
+def layer_l1(model1, model2):
     pass
