fb085d0c3ac5135b2b0647db764ebf63383c3ef4
liuup <akarealup@gmail.com>
2024-10-07 21:51:04 +0800

    add the lsc_cfk

--- unlearning.py
+++ unlearning.py
@@ -31,7 +31,7 @@ def adjust_cfk(model, unlearn_k):
                 break
 
 
-# lsc_euk 按照层间偏移从大到小的顺序，再训练偏移最大的k层
+# lsc_euk 按照层间偏移从大到小的顺序，重新训练偏移最大的k层
 # 根据model12_layer_cossim_overall，也就是model1和model2对比得到偏移最大的unlearn_k层
 def adjust_lsc_euk(model, unlearn_k, layer_cossim_once):
     layers = [name for name, _ in model.named_parameters()]
@@ -50,8 +50,27 @@ def adjust_lsc_euk(model, unlearn_k, layer_cossim_once):
     for name, param in model.named_parameters():
         if name not in unlearn_k_layers:
             param.requires_grad = False # 取消梯度
+        else:
+            nn.init.normal_(param, mean=0, std=0.1)
 
 
-# lsc_cfk 按照层间偏移从大到小的顺序，随机初始化偏移最大的k层
-def adjust_lsc_cfk(model, unlearn_k, layers):
-    pass
+# lsc_cfk 按照层间偏移从大到小的顺序，继续训练偏移最大的k层
+def adjust_lsc_cfk(model, unlearn_k, layer_cossim_once):
+    layers = [name for name, _ in model.named_parameters()]
+
+    # 从小到大进行排序，从最后一个epoch中找出偏移量最大的unlearn_k层
+    unlearn_k_data = sorted(layer_cossim_once[-1], key=lambda x: x[1])[:unlearn_k]
+    tmp = [name for name, _ in unlearn_k_data]
+
+    unlearn_k_layers = []
+    for t in tmp:
+        if t+".weight" in layers:
+            unlearn_k_layers.append(t+".weight")
+        if t+".bias" in layers:
+            unlearn_k_layers.append(t+".bias")
+
+    for name, param in model.named_parameters():
+        if name not in unlearn_k_layers:
+            param.requires_grad = False # 取消梯度
+
+    
